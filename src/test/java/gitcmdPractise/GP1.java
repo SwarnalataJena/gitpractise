d1c5cd8032b7afe5270dcbedd3c7295cfc42404c
SwarnalataJena <jenaswarnalata600@gmail.com>
2022-09-14 13:15:48 +0530

    new file added

---
+++ src/test/java/gitcmdPractise/GP1.java
@@ -0,0 +1,8 @@
+package gitcmdPractise;
+
+public class GP1 {
+	public static void main(String[] args) {
+		System.out.println("eclipse is talking with github through gitbash");
+	}
+
+}
---
+++ src/test/java/gitcmdPractise/GP2.java
@@ -0,0 +1,8 @@
+package gitcmdPractise;
+
+public class GP2 {
+	public static void main(String[] args) {
+		System.out.println("eclipse is talking with github through gitbash");
+	}
+
+}
